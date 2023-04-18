281f7f0024e1b8ef3109c6e50feda70faf999043
jmello <julio_mello@hacienda.gov.py>
2023-04-18 14:46:42 -0400

    Se actualiza estructura para realizar pruebas del algoritmo

--- src/py/com/pdi/QHELC.java
+++ src/py/com/pdi/QHELC.java
@@ -1,5 +1,21 @@
 package py.com.pdi;
 
-public class QHELC {
+import ij.ImagePlus;
+import ij.plugin.filter.PlugInFilter;
+import ij.process.ImageProcessor;
+
+public class QHELC implements PlugInFilter{
+
+	@Override
+	public void run(ImageProcessor ip) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int setup(String args, ImagePlus im) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
 }
--- src/py/com/pdi/TestQHELC.java
+++ src/py/com/pdi/TestQHELC.java
@@ -1,10 +1,50 @@
 package py.com.pdi;
 
+import java.io.File;
+
+import ij.IJ;
+import ij.ImagePlus;
+import ij.process.ImageProcessor;
+
 public class TestQHELC {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		String sCarpAct = System.getProperty("user.dir").concat("\\imagenes");
+		File carpeta = new File(sCarpAct);
+		String[] listado = carpeta.list();
 
+		if (listado == null || listado.length == 0) {
+			System.out.println("No hay elementos dentro de la carpeta actual");
+			return;
+		} else {
+			for (int i = 0; i < listado.length; i++) {
+				System.out.println(listado[i]);
+				String ruta = sCarpAct.concat("\\").concat(listado[i]);
+				System.out.println(ruta);
+				ImagePlus im = IJ.openImage(ruta); // carga la imagen
+				im.show(); // Muestra la imagen original
+
+				ImagePlus im2 = im.duplicate();
+
+				// Inicio del algoritmo
+				long time_start = System.currentTimeMillis();
+
+				// Agregar el algoritmo de QHELC
+				QHELC qhelc = new QHELC();
+				ImageProcessor ip = im2.getProcessor();
+				qhelc.run(ip);
+
+				// Fin del tiempo en milisegundos
+				long time_end = System.currentTimeMillis();
+				long time = time_end - time_start;
+
+				im2.show();
+
+				// Guardar los resultados
+				String rGuardar = System.getProperty("user.dir").concat("\\resultados\\QHELC\\").concat(listado[i]);
+				System.out.println(rGuardar);
+				IJ.saveAs(im2, "tif", rGuardar);
+			}
+		}
 	}
-
 }
